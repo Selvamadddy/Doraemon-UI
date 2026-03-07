921b544fa192594a7f9f829cad7945114d4397c3
selvamaddy1998 <selvamaddy1998@gmail.com>
2026-03-07 11:29:40 +0530

    Deployment preparation

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,10 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default ({ mode }: { mode: string }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+export default () => {
 
   return defineConfig({
     base: "/",
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/apim': {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          rewrite: (path: string) => path.replace(/^\/apim/, ''),
-        },
-        '/quotes': {
-          target: 'https://zenquotes.io',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/quotes/, ''),
-        },
-      },
-    },
+    plugins: [react()]
   })
 }
